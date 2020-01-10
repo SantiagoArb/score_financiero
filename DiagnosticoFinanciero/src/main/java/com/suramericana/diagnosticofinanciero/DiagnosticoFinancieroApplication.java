194ae2d53ece41a03bd2b1a615c1eb0919e777af
SantiagoArb <37001110+SantiagoArb@users.noreply.github.com>
2020-01-10 08:25:47 -0500

    Add files via upload

---
+++ DiagnosticoFinanciero/src/main/java/com/suramericana/diagnosticofinanciero/DiagnosticoFinancieroApplication.java
@@ -0,0 +1,13 @@
+package com.suramericana.diagnosticofinanciero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosticoFinancieroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagnosticoFinancieroApplication.class, args);
+	}
+
+}
---
+++ DiagnosticoFinanciero/src/test/java/com/suramericana/diagnosticofinanciero/DiagnosticoFinancieroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suramericana.diagnosticofinanciero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagnosticoFinancieroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
